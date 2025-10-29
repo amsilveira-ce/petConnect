58b62eb069e0db2f4442fd5e4a255b69aa6d0cb3
Amanda da Silveira <Amanda.da.Silveira@ibm.com>
2025-10-28 22:49:44 -0300

    Estrutura básica para metódos de autenticação

---
+++ backend/controllers/auth-controllers.js
@@ -0,0 +1,19 @@
+
+// --- Controladores de Autenticação ---
+// utilizam o modelo User para gerenciar usuários e ongs
+
+export const signup = async (req, res) => {
+    
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
+
+export const verifyEmail = async (req, res) => {};
+
+export const forgotPassword = async (req, res) => {};
+
+export const resetPassword = async (req, res) => {};
+
+export const checkAuth = async (req, res) => {};
